b9de56001a1dd315aecb1b9cb825f731ddbdf3d0
Kacper Margol <kacpermargol@gmail.com>
2025-05-20 06:49:46 +0200

    Add text input component and integrate into job index view: create TextInput component for reusable input fields, update package.json to include @tailwindcss/forms, and enhance job index view with search and salary input fields.

--- job-board/resources/views/jobs/index.blade.php
+++ job-board/resources/views/jobs/index.blade.php
@@ -4,6 +4,25 @@
         'Jobs' => route('jobs.index'),
     ]" />
 
+    <x-card class="mb-4 text-sm">
+        <div class="mb-4 grid grid-cols-2 gap-4">
+            <div>
+                <div class="mb-1 font-semibold">Search</div>
+                <x-text-input name="search" placeholder="Search..." value='' />
+            </div>
+            <div>
+                <div class="mb-1 font-semibold">Salary</div>
+                <div class="flex space-x-2">
+                    <x-text-input name="min_salary" placeholder="From" value='' />
+                    <x-text-input name="max_salary" placeholder="To" value='' />
+                </div>
+            </div>
+            <div>3</div>
+            <div>4</div>
+
+        </div>
+    </x-card>
+
     @foreach ($jobs as $job)
         <x-job-card :$job >
             <div>
